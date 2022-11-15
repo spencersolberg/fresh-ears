b9d0979131f74400b69fd2cd80bd5e6ca711e61d
Spencer <other.spencersolberg@outlook.com>
2022-11-15 13:14:52 -0500

    Initial commit

---
+++ islands/Game.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "preact/hooks";
+// import * as Tone from "https://esm.sh/tone?module";
+import * as Tone from "npm:tone";
+
+export default function Game() {
+  const [synth, setSynth] = useState(new Tone.Synth().toDestination());
+  const [notes, setNotes] = useState(["C4"]);
+
+  const playInterval = () => {
+    // Gets two random notes from the notes array
+    const first = notes[Math.floor(Math.random() * notes.length)];
+    const second = notes[Math.floor(Math.random() * notes.length)];
+
+    // Plays the first note
+    synth.triggerAttackRelease(first, "8n");
+
+    // Plays the second note after 1 second
+    setTimeout(() => {
+      synth.triggerAttackRelease(second, "8n");
+    }, 1000);
+  };
+
+  return (
+    <>
+      <div class="flex flex-col">
+        <h1 class="text-6xl font-medium m-auto">Fresh Ears</h1>
+        <button class="text-6xl cursor-pointer outline-none transform-gpu hover:scale-105 transition-transform active:scale-95 focus:outline-none">
+          🎶
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,28 @@
+import { Head } from "$fresh/runtime.ts";
+import Game from "../islands/Game.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class="my-6">
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
+        </p>
+        <p class="my-6">
+          For more information, check out the{" "}
+        </p>
+        <p class="text-5xl">BIG TEXT</p>
+        <Game />
+      </div>
+    </>
+  );
+}
